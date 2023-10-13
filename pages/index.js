9b0796f390e61a28a1070d1cc80e66ef22eec37a
nicholas-callen <nicholascallen13@gmail.com>
2023-10-12 23:07:53 -0700

    testing content

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Nicholas Callen" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Testing in <code>pages/index.js</code>
         </p>
       </main>
 
